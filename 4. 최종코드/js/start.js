ab41cbc6e4ae4bfa498aba62a6bd613f46af7fbc
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-03-28 23:06:29 +0900

    [ADD]메인페이지완료

--- 4. 최종코드/js/start.js
+++ 4. 최종코드/js/start.js
@@ -113,11 +113,11 @@ function goNext(qIdx){
   var status = document.querySelector('.statusBar');
   status.style.width = (100/endPoint) * (qIdx+1) + '%';
 }
-
+//시작 애니메이션 
 function begin(){
   main.style.WebkitAnimation = "fadeOut 1s";
   main.style.animation = "fadeOut 1s";
-  setTimeout(() => {
+  setTimeout(() => { 
     qna.style.WebkitAnimation = "fadeIn 1s";
     qna.style.animation = "fadeIn 1s";
     setTimeout(() => {
